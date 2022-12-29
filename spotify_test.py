8e5da322a077dddef5baef4090731fb29e15318c
ManodnyaG <115745665+ManodnyaG@users.noreply.github.com>
2022-12-28 22:57:09 -0700

    Update spotify_test.py

--- spotify_test.py
+++ spotify_test.py
@@ -1,50 +1,64 @@
-{
-  "metadata": {
-    "language_info": {
-      "codemirror_mode": {
-        "name": "python",
-        "version": 3
-      },
-      "file_extension": ".py",
-      "mimetype": "text/x-python",
-      "name": "python",
-      "nbconvert_exporter": "python",
-      "pygments_lexer": "ipython3",
-      "version": "3.8"
-    },
-    "kernelspec": {
-      "name": "python",
-      "display_name": "Python (Pyodide)",
-      "language": "python"
-    }
-  },
-  "nbformat_minor": 4,
-  "nbformat": 4,
-  "cells": [
-    {
-      "cell_type": "code",
-      "source": "import pandas as pd \nimport numpy as np",
-      "metadata": {
-        "trusted": true
-      },
-      "execution_count": null,
-      "outputs": []
-    },
-    {
-      "cell_type": "code",
-      "source": "df1 = pd.read_csv('test.csv')",
-      "metadata": {
-        "trusted": true
-      },
-      "execution_count": null,
-      "outputs": []
-    },
-    {
-      "cell_type": "code",
-      "source": "import streamlit as st\nst.title('Spotify Data Explorer')\nst.text('This is a web app to allow exploration of Spotify Charts Data')\n\nimport pandas as pd \nimport numpy as np\n\nfrom pyspark.sql import SparkSession\nimport pyspark.sql.types as t\nimport pyspark.sql.functions as f\nimport seaborn as sns\n\nimport matplotlib.pyplot as plt\n\nadd_selectbox = st.sidebar.selectbox(\n    \"What would you like to look at first\",\n    (\"Big data analysis\", \"Songs\",\"About\")\n)\n\n#df1 = pd.read_csv('/Users/csuftitan/Downloads/charts.csv')\ndf1 = pd.read_csv('https://drive.google.com/file/d/1fUjXJI49cMAsYlnU_l78gKOM5eKqx0os/view?usp=share_link')\ndf1.head()\nst.header('Header of Dataframe')\nst.write(df1.head())\n\nspark = SparkSession.builder.appName(\"spark_app\").getOrCreate()\n\n#df = spark.read.csv(path='/Users/csuftitan/Downloads/charts.csv', inferSchema=True, header=True)\ndf = spark.read.csv(path='https://drive.google.com/file/d/1fUjXJI49cMAsYlnU_l78gKOM5eKqx0os/view?usp=share_link', inferSchema=True, header=True)\n\n\ndf = df.withColumn(\"rank\", f.col(\"rank\").cast(t.LongType())).withColumn(\"date\", f.col(\"date\").cast(t.DateType())).withColumn(\"streams\", f.col(\"streams\").cast(t.IntegerType()))\ndf = df.na.drop()\ndf.count()\ndf.registerTempTable(\"charts\")\n",
-      "metadata": {},
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+
+import streamlit as st
+st.title('Spotify Data Explorer')
+st.text('This is a web app to allow exploration of Spotify Charts Data')
+
+import pandas as pd 
+import numpy as np
+
+from pyspark.sql import SparkSession
+import pyspark.sql.types as t
+import pyspark.sql.functions as f
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+add_selectbox = st.sidebar.selectbox(
+    "What would you like to look at first",
+    ("Big data analysis", "Songs","About")
+)
+
+#df1 = pd.read_csv('/Users/csuftitan/Downloads/charts.csv')
+df1 = pd.read_csv('test.csv')
+df1.head()
+st.header('Header of Dataframe')
+st.write(df1.head())
+
+spark = SparkSession.builder.appName("spark_app").getOrCreate()
+
+#df = spark.read.csv(path='/Users/csuftitan/Downloads/charts.csv', inferSchema=True, header=True)
+df = spark.read.csv('test.csv', inferSchema=True, header=True)
+
+
+df = df.withColumn("rank", f.col("rank").cast(t.LongType())).withColumn("date", f.col("date").cast(t.DateType())).withColumn("streams", f.col("streams").cast(t.IntegerType()))
+df = df.na.drop()
+df.count()
+df.registerTempTable("charts")
+
+n_song = st.selectbox(label='Please select a song', options=df1.title.unique()) 
+    
+query2 = ("SELECT region,count(trend) as trend FROM charts WHERE title like '{}' and trend like '%MOVE_UP%' group by region;").format(n_song)
+
+art = spark.sql(query2).toPandas()
+
+reg = art.region.tolist()
+reg = reg[:30]
+move_up = art.trend.tolist()
+move_up = move_up[:30]
+
+
+
+
+df1 = pd.DataFrame({
+  'move_up': move_up,
+  'region': reg
+})
+
+df1
+st.subheader('Below graph represents the number of times the selected song has moved up in position in perticular region')
+df1
+
+if check:
+    st.code('''"SELECT region,count(trend) as trend FROM charts WHERE title like '{}' and trend like '%MOVE_UP%' group by region;").format(n_song)''')
+df1 = df1.rename(columns={'region':'index'}).set_index('index')    
+st.bar_chart(df1)
